c23e5ca6bc6465b5493de9c43fbde104fcf18bd2
BasLaa <baslaarakker@gmail.com>
2023-03-15 13:22:51 +0100

    simple skeleton

--- run_mario.py
+++ run_mario.py
@@ -1,47 +1,14 @@
 from nes_py.wrappers import JoypadSpace
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
-
-import keyboard
-import time 
-
-env = gym_super_mario_bros.make('SuperMarioBros2-v0')
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
-
-# actions for very simple movement
-# SIMPLE_MOVEMENT = [
-#     ['NOOP'],
-#     ['right'],
-#     ['right', 'A'],
-#     ['right', 'B'],
-#     ['right', 'A', 'B'],
-#     ['A'],
-#     ['left'],
-# ]
 
 done = True
 for step in range(5000):
     if done:
         state = env.reset()
-    if keyboard.is_pressed('up') and keyboard.is_pressed('right') and keyboard.is_pressed('space'):
-        act = 4
-    if keyboard.is_pressed('up') and keyboard.is_pressed('right'):
-        act = 2
-    elif keyboard.is_pressed('space') and keyboard.is_pressed('right'):
-        act = 3
-    elif keyboard.is_pressed('up'):
-        act = 5
-    elif keyboard.is_pressed('right'):
-        act = 1
-    elif keyboard.is_pressed('left'):
-        act = 6
-    else:
-        act = 0
-    state, reward, done, info = env.step(act)
-    print(reward)
+    state, reward, done, info = env.step(env.action_space.sample())
     env.render()
-
-    time.sleep(1/60)
-
 
 env.close()
