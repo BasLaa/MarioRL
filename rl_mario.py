9e7f20e849cae66250e70ca071e8631116981210
nic0gin <nikolai.gini@gmail.com>
2023-03-15 17:06:39 +0100

    added callback

--- rl_mario.py
+++ rl_mario.py
@@ -1,11 +1,36 @@
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import RIGHT_ONLY, SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
 from stable_baselines3 import PPO
+from stable_baselines3.common.callbacks import BaseCallback
+
 import make_env
 
-import time 
+import time
 import os
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+class TrainAndLoggingCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(self.save_path, 'best_model_{}'.format(self.n_calls))
+            self.model.save(model_path)
+
+        return True
+
+
+callback = TrainAndLoggingCallback(check_freq=10000, save_path='./train')
 
 
 def run_model(pretrained=False, model_name="mario_rl"):
@@ -22,7 +47,7 @@ def run_model(pretrained=False, model_name="mario_rl"):
     else:
         print("Training new model...")
         model = PPO("MlpPolicy", env, verbose=1, n_epochs=10, n_steps=3000, batch_size=100)
-        model.learn(total_timesteps=4000,)
+        model.learn(total_timesteps=4000, callback=callback)
         model.save(f"models/{model_name}")
 
     vec_env = model.get_env()
@@ -32,7 +57,8 @@ def run_model(pretrained=False, model_name="mario_rl"):
         action, _ = model.predict(state, deterministic=False)
         state, reward, done, info = vec_env.step(action)
         vec_env.render()
-        time.sleep(1/120)
+        time.sleep(1 / 120)
+
 
 # Models are saved in folder "models"
 if __name__ == "__main__":
