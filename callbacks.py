8460b047f13cd303984701b28bced98a3db26e3b
BasLaa <baslaarakker@gmail.com>
2023-03-15 18:59:42 +0100

    final model flow tonight

---
+++ callbacks.py
@@ -0,0 +1,52 @@
+from tqdm.auto import tqdm
+from stable_baselines3.common.callbacks import BaseCallback
+import os
+
+class ProgressBarCallback(BaseCallback):
+    """
+    :param pbar: (tqdm.pbar) Progress bar object
+    """
+    def __init__(self, pbar):
+        super(ProgressBarCallback, self).__init__()
+        self._pbar = pbar
+
+    def _on_step(self):
+        # Update the progress bar:
+        self._pbar.n = self.num_timesteps
+        self._pbar.update(0)
+
+# this callback uses the 'with' block, allowing for correct initialisation and destruction
+class ProgressBarManager(object):
+    def __init__(self, total_timesteps): # init object with total timesteps
+        self.pbar = None
+        self.total_timesteps = total_timesteps
+        
+    def __enter__(self): # create the progress bar and callback, return the callback
+        self.pbar = tqdm(total=self.total_timesteps)
+            
+        return ProgressBarCallback(self.pbar)
+
+    def __exit__(self, exc_type, exc_val, exc_tb): # close the callback
+        self.pbar.n = self.total_timesteps
+        self.pbar.update(0)
+        self.pbar.close()
+
+
+class TrainAndLoggingCallback(BaseCallback):
+
+    def __init__(self, check_freq, save_path, name_prefix="", verbose=1):
+        super(TrainAndLoggingCallback, self).__init__(verbose)
+        self.check_freq = check_freq
+        self.save_path = save_path
+        self.name_prefix = name_prefix
+
+    def _init_callback(self):
+        if self.save_path is not None:
+            os.makedirs(self.save_path, exist_ok=True)
+
+    def _on_step(self):
+        if self.n_calls % self.check_freq == 0:
+            model_path = os.path.join(self.save_path, self.name_prefix+'_{}'.format(self.n_calls))
+            self.model.save(model_path)
+
+        return True
--- rl_mario.py
+++ rl_mario.py
@@ -1,34 +1,23 @@
 import gym_super_mario_bros
 from gym_super_mario_bros.actions import RIGHT_ONLY, SIMPLE_MOVEMENT, COMPLEX_MOVEMENT
 from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import CheckpointCallback, BaseCallback
+from stable_baselines3.common.callbacks import CallbackList
 from stable_baselines3.common.logger import configure
 
+import callbacks
 import make_env
 
 import time 
 import os
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
+N_TIMESTEPS = 4000000
+LEARNING_RATE = 0.0001
+GAMMA = 0.95
+N_EPOCHS = 40
+N_STEPS = 2048
+BATCH_SIZE = 64
 
-class TrainAndLoggingCallback(BaseCallback):
-
-    def __init__(self, check_freq, save_path, name_prefix="", verbose=1):
-        super(TrainAndLoggingCallback, self).__init__(verbose)
-        self.check_freq = check_freq
-        self.save_path = save_path
-        self.name_prefix = name_prefix
-
-    def _init_callback(self):
-        if self.save_path is not None:
-            os.makedirs(self.save_path, exist_ok=True)
-
-    def _on_step(self):
-        if self.n_calls % self.check_freq == 0:
-            model_path = os.path.join(self.save_path, self.name_prefix+'_{}'.format(self.n_calls))
-            self.model.save(model_path)
-
-        return True
 
 def run_model(env, pretrained=False, model_name="mario_rl", callback=None, logger=None):
 
@@ -40,9 +29,11 @@ def run_model(env, pretrained=False, model_name="mario_rl", callback=None, logge
         return
     else:
         print("Training new model...")
-        model = PPO("MlpPolicy", env, tensorboard_log="./tensorboard_log/", verbose=1, n_epochs=10, n_steps=2048, batch_size=64)
+        model = PPO("MlpPolicy", env, verbose=1, learning_rate=LEARNING_RATE, gamma=GAMMA, n_epochs=N_EPOCHS, n_steps=N_STEPS, batch_size=BATCH_SIZE)
         model.set_logger(logger)
-        model.learn(total_timesteps=15000, callback=callback)
+        with callbacks.ProgressBarManager(N_TIMESTEPS) as progress_callback: # this the garanties that the tqdm progress bar closes correctly
+            final_callback = CallbackList([callback, progress_callback])
+            model.learn(total_timesteps=N_TIMESTEPS, callback=final_callback)
         model.save(f"models/{model_name}")
 
     vec_env = model.get_env()
@@ -67,7 +58,7 @@ if __name__ == "__main__":
     # stop_train_callback = StopTrainingOnNoModelImprovement(max_no_improvement_evals=30, min_evals=5, verbose=1)
     # eval_callback = EvalCallback(env, eval_freq=1000, callback_after_eval=stop_train_callback, verbose=1)
 
-    checkpoint_callback = TrainAndLoggingCallback(
+    checkpoint_callback = callbacks.TrainAndLoggingCallback(
         check_freq=5000,
         save_path="./model_logs/",
         name_prefix="ppo_model",
@@ -78,4 +69,8 @@ if __name__ == "__main__":
     new_logger = configure(logger_path, ["stdout", "csv", "tensorboard"])
 
 
+    # TRAINING MODEL
     run_model(env, model_name="ppo_model_total", pretrained=False, callback=checkpoint_callback, logger=new_logger)
+
+    # RUNNING TRAINED MODEL
+    # run_model(env=env, pretrained=True, model_name="ppo_model_total")
